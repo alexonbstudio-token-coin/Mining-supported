f9c9db405e6b8b24c629dbf5241d08a816a17c47
AlexonbStudio <65239996+alexonbstudio@users.noreply.github.com>
2021-08-26 15:26:45 +0200

    create public
    testing some command line and testing is works 'll

---
+++ ubuntu-btc.sh
@@ -0,0 +1,22 @@
+/bin/sh
+###################
+## SH create by Alexonb.Studio
+## Mining Works and testing Bitcoin
+## Approuvate 28/08/2021 to 3h21 PM (GMT+002 Paris)
+###################
+
+
+sudo -i
+
+apt update && apt upgrade -y
+
+
+apt install -y libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm autoconf gcc make git libtool libjansson-dev libudev libusb-1.0-0-dev apt-file make
+
+git clone https://github.com/pooler/cpuminer.git
+cd cpuminer
+./autogen.sh
+./configure CFLAGS="-03"
+make
+
+ ./minerd -o stratum+tcp://yourpool.TLD:123 -u USERNAME -p PASSWORD
